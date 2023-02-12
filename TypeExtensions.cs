8a0965fddc5e33e92bd420f29012a294c8784e01
Anthony Loffredo <loffredo.ap@gmail.com>
2023-02-12 01:28:40 -0700

    Autocommit
    Autocommit

--- TypeExtensions.cs
+++ TypeExtensions.cs
@@ -272,37 +272,7 @@ namespace Penguin.Reflection.Extensions
             }
         }
 
-        /// <summary>
-        /// Returns a string that can be used to declare a type in code (ex System.Collections.Generic.List&lt;System.String&gt;)
-        /// </summary>
-        /// <param name="type">The type to get the declaration for</param>
-        /// <returns>The type declaration</returns>
-        public static string GetDeclaration(this Type type)
-        {
-            if (type is null)
-            {
-                throw new ArgumentNullException(nameof(type));
-            }
 
-            if (!type.GetGenericArguments().Any())
-            {
-                return type.FullName;
-            }
-            else
-            {
-                StringBuilder sb = new();
-
-                _ = sb.Append(type.FullName.To("`"));
-
-                _ = sb.Append('<');
-
-                _ = sb.Append(string.Join(",", type.GetGenericArguments().Select(t => t.GetDeclaration())));
-
-                _ = sb.Append('>');
-
-                return sb.ToString();
-            }
-        }
 
         /// <summary>
         /// Resolves the default value for a generic type. Dont know why this exists
